a69165352123498a3894e62af2edbed2762ddff4
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-08-29 14:01:47 -0400

    Push

---
+++ app/src/main/java/themastergeneral/chivalryisdead/MainActivity.java
@@ -0,0 +1,45 @@
+package themastergeneral.chivalryisdead;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+    final Activity activity = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView = (WebView) findViewById(R.id.webview);
+        mywebView.setWebChromeClient(new WebChromeClient());
+        mywebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mywebView.getSettings().setJavaScriptEnabled(true);
+        mywebView.getSettings().setDomStorageEnabled(true);
+        mywebView.loadUrl("http://chivalryisdead.x10.mx/");
+    }
+
+    //Code For Back Button
+    @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack())
+        {
+            mywebView.goBack();
+        }
+
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+}
