bdc7df728aff4d4c288e2fb52657e98d40288796
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-15 11:20:21 -0400

    1.0.8.26

--- app/src/main/java/themastergeneral/chivalryisdead/AppData.java
+++ app/src/main/java/themastergeneral/chivalryisdead/AppData.java
@@ -5,8 +5,8 @@ package themastergeneral.chivalryisdead;
  */
 public class AppData
 {
-    public static String UserAgent = "CID-App-Android";
+    public static String UserAgent = "App-Android";
     public static String URL = "http://chivalryisdead.x10.mx/";
     public static String VersionChecker = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/cid_app.json";
-    public static String LatestDownload = "http://chivalryisdead.x10.mx/downloads/latest_app.php";
+    public static String LatestDownload = "http://chivalryisdead.x10.mx/downloads/latest_app_uncache.php";
 }
