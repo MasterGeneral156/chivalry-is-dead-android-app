72c617945e592593a498d5079c5ac6eeba91f7fd
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-16 10:47:10 -0400

    1.0.8.26

--- app/src/main/java/themastergeneral/chivalryisdead/AppData.java
+++ app/src/main/java/themastergeneral/chivalryisdead/AppData.java
@@ -5,7 +5,7 @@ package themastergeneral.chivalryisdead;
  */
 public class AppData
 {
-    public static String UserAgent = "App-Android";
+    public static String UserAgent = "CEngine-App-Android";
     public static String URL = "http://chivalryisdead.x10.mx/";
     public static String VersionChecker = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/cid_app.json";
     public static String LatestDownload = "http://chivalryisdead.x10.mx/downloads/latest_app_uncache.php";
--- app/src/main/java/themastergeneral/chivalryisdead/VersionChecker.java
+++ app/src/main/java/themastergeneral/chivalryisdead/VersionChecker.java
@@ -16,6 +16,9 @@ public class VersionChecker
         WVersionManager versionManager = new WVersionManager(activity);
         versionManager.setVersionContentUrl(AppData.VersionChecker);    //Update URL
         versionManager.setUpdateUrl(AppData.LatestDownload);            //Download URL
+        versionManager.setUpdateNowLabel("Update");                     //Do this because friendly.
+        versionManager.setIgnoreThisVersionLabel("Ignore");             //Do this because friendly.
+        versionManager.setRemindMeLaterLabel("Remind Later");           //Do this because friendly.
         versionManager.checkVersion();
         versionManager.setOnReceiveListener(new OnReceiveListener()
         {
