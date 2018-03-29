6bbf5c1db7c498b15a9689eb48214f43c2a23c50
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-03-28 21:33:28 -0400

    App 1.1.2.35

--- app/src/main/java/com/chivalryisdeadgame/chivalryisdead/AppData.java
+++ app/src/main/java/com/chivalryisdeadgame/chivalryisdead/AppData.java
@@ -1,8 +1,10 @@
 package com.chivalryisdeadgame.chivalryisdead;
 
+import android.os.Build;
+
 public class AppData
 {
-    public static String UserAgent = "CEngine-App-Android";
+    public static String UserAgent = "CEngine-App-Android on Android " + Build.VERSION.RELEASE;
     public static String URL = "https://www.chivalryisdeadgame.com/";
     public static String VersionChecker = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/cid_app.json";
     public static String LatestDownload = "https://www.chivalryisdeadgame.com/downloads/latest_app_uncache.php";
--- app/src/main/java/com/chivalryisdeadgame/chivalryisdead/MainActivity.java
+++ app/src/main/java/com/chivalryisdeadgame/chivalryisdead/MainActivity.java
@@ -1,15 +1,12 @@
 package com.chivalryisdeadgame.chivalryisdead;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import java.util.logging.Handler;
 
 
 public class MainActivity extends Activity
@@ -25,18 +22,30 @@ public class MainActivity extends Activity
         mywebView.getSettings().setDatabaseEnabled(true);
         mywebView.getSettings().setJavaScriptEnabled(true);
         mywebView.getSettings().setDomStorageEnabled(true);
+        mywebView.getSettings().setAllowFileAccess(true);
+        mywebView.getSettings().setAllowFileAccessFromFileURLs(true);
+        mywebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
         mywebView.getSettings().setUserAgentString(AppData.UserAgent);
         mywebView.getSettings().setAppCacheEnabled(true);
-        mywebView.loadUrl(AppData.URL);
+        //Allow mixed content on Lollipop and above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            mywebView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         mywebView.setWebChromeClient(new WebChromeClient());
         mywebView.setWebViewClient(new WebViewClient() {
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+                if (url != null && url.startsWith("cid://")) {
+                    String url2 = url.replace("cid://", "file:///android_asset/");
+                    mywebView.loadUrl(url2);
+                    return true;
+                } else {
+                    mywebView.loadUrl(url);
+                    return true;
+                }
             }
         });
+        mywebView.loadUrl(AppData.URL);
     }
 
     @Override
