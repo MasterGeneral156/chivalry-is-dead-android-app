91266266ef598829cbb246d0833b5b72f6b0e0f4
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-04-02 18:41:05 -0400

    local ip

--- app/src/main/java/com/chivalryisdeadgame/chivalryisdead/AppData.java
+++ app/src/main/java/com/chivalryisdeadgame/chivalryisdead/AppData.java
@@ -5,7 +5,7 @@ import android.os.Build;
 public class AppData
 {
     public static String UserAgent = "CEngine-App-Android on Android " + Build.VERSION.RELEASE;
-    public static String URL = "https://www.chivalryisdeadgame.com/";
+    public static String URL = "192.168.128.151/cid/login.php";
     public static String VersionChecker = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/cid_app.json";
     public static String LatestDownload = "https://www.chivalryisdeadgame.com/downloads/latest_app_uncache.php";
 }
